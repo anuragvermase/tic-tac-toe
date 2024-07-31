935a9d36668e851a3f72f1d3b83166e3ecac2147
anurag verma <anuragver689@gmail.com>
2024-08-01 00:14:46 +0530

    add

---
+++ tic-tac-toe/src/components/Grid/Grid.jsx
@@ -0,0 +1,15 @@
+import Card from "../Card/Card";
+import "./Grid.css"
+function Grid({numberOfCards}){
+    console.log(numberOfCards);
+    return (
+        <div className="grid">
+        {Array(numberOfCards).fill(<Card />).map((el,idx) => {
+            return <Card key={idx}/>
+
+        })}
+        </div>
+    )
+
+}
+export default Grid;
