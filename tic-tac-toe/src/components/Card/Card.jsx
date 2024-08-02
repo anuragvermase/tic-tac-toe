ff1215f61981036b5bd1dc4977d469b5176c92e8
anurag verma <anuragver689@gmail.com>
2024-08-03 00:07:27 +0530

    add

--- tic-tac-toe/src/components/Card/Card.jsx
+++ tic-tac-toe/src/components/Card/Card.jsx
@@ -1,11 +1,19 @@
 import Icon from "../Icon/Icon";
 import "./Card.css"
-function Card({ iconName} ){
+
+function Card({ onPlay, player, index} ){
+
+    let icon = <Icon />
+    if(player == "X"){
+        icon = <Icon name = {"cross"} />
+    } else if(player == "O"){
+        icon = <Icon name = {"circle"} />
+    }
+
     return (
-        <div className='card'>
-            <Icon name = {iconName}/>
+        <div className='card' onClick={() => onPlay(index)}>
+            {icon}
         </div>
-        
     );
 } 
 
--- tic-tac-toe/src/components/Grid/Grid.jsx
+++ tic-tac-toe/src/components/Grid/Grid.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Card from "../Card/Card";
 import "./Grid.css"
 function Grid({numberOfCards}){
-    console.log(numberOfCards);
-    return (
-        <div className="grid">
-        {Array(numberOfCards).fill(<Card />).map((el,idx) => {
-            return <Card key={idx}/>
+    // console.log(numberOfCards);
+    const [turn, setTurn] = useState(true); // true -> O, false-> X
+    const [board, setBoard] = useState(Array(numberOfCards).fill("")); // ["","","","","","","","",""]
 
+    function play(index){
+        console.log("move played",index);
+        if(turn == true){
+            board[index] = "O";
+        }else{
+            board[index] = "X"
+        }
+        setBoard([...board]);
+        setTurn(!turn);
+    }
+
+    return (
+        <>
+
+        <h1 className="turn-highlight"> Current Turn : {(turn) ? "O" : "X"} </h1>
+
+        <div className="grid">
+        {board.map((value, idx) => {
+            return <Card onPlay ={play} player={value} key={idx} index={idx}/>              
         })}
         </div>
-    )
 
+        </>
+    )
 }
 export default Grid;
